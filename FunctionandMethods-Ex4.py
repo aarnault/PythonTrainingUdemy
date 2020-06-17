2f4d5f9717ce460add775725e061546a6e5f7853
audrey.arnault <audrey-arnault@web-address.fr>
2020-06-17 10:10:46 +0100

    end2, does not want to work....but works on jupyter

--- FunctionandMethods-Ex4.py
+++ FunctionandMethods-Ex4.py
@@ -4,11 +4,10 @@ unique_list ([1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 5])
 
 
 def unique_list(lst):
-    seen_numbers=[]
+    seen_numbers = []
     for nb in lst:
         if nb not in seen_numbers:
             seen_numbers.append(nb)
     return seen_numbers
 
 unique_list ([1,1,1,1,2,2,3,3,3,3,4,5])
-
