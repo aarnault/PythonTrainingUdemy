5756796cdeec855c8d64660c5e1393f261c8e396
audrey.arnault <audrey-arnault@web-address.fr>
2020-06-17 09:50:02 +0100

    1st draft

---
+++ FunctionandMethods-Ex4.py
@@ -0,0 +1,12 @@
+def unique_list(lst):
+    x=0
+    new_list=0
+    for nb in lst:
+        if nb[x] = nb[x-1]:
+            new_list.append()
+            x += 1
+        else:
+            pass
+    print('new_list')
+
+unique_List([1,1,1,1,2,2,3,3,3,3,4,5])
