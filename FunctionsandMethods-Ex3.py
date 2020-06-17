de35d9b65df51c036a96044111f2eafdca03a4f3
audrey.arnault <audrey-arnault@web-address.fr>
2020-06-17 09:38:51 +0100

    End

--- FunctionsandMethods-Ex3.py
+++ FunctionsandMethods-Ex3.py
@@ -1,14 +1,30 @@
 def up_low(s):
-    word=s.split()
-    y=0
-    z=0
-    for x in word:
-        if x.isupper():
-            y=y+1
-        elif x.islower():
-            z=z+1
+    lowercase = 0
+    uppercase = 0
+    for char in s:
+        if char.isupper():
+            uppercase += 1
+        elif char.islower():
+            lowercase += 1
         else:
             pass
+    print(f"Original String: {s}")
+    print(f"Number of Lower Case: {lowercase}")
+    print(f"Number of Upper Case: {uppercase}")
+s = "Hello Mr. Rogers, how are you this fine Tuesday?"
+up_low(s)
 
-    print(f”Number of Upper case characters: {y}”)
-  print(f”Number of Upper case characters: {z}”)
+def up_low(s):
+    d={"upper":0, "lower":0}
+    for char in s:
+        if char.isupper():
+            d["upper"] += 1
+        elif char.islower():
+            d["lower"] += 1
+        else:
+            pass
+    print(f"Original String: {s}")
+    print(f"Number of Lower Case: {d['lower']}")
+    print(f"Number of Upper Case: {d['upper']}")
+s = "Hello Mr. Rogers, how are you this fine Tuesday?"
+up_low(s)
