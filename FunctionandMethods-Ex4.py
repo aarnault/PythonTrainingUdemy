f70312872859b2a1a9fda7d6fec448d8c6cc70cb
audrey.arnault <audrey-arnault@web-address.fr>
2020-06-17 10:07:38 +0100

    2 possibilities

--- FunctionandMethods-Ex4.py
+++ FunctionandMethods-Ex4.py
@@ -1,12 +1,14 @@
-def unique_list(lst):
-    x=0
-    new_list=0
-    for nb in lst:
-        if nb[x] = nb[x-1]:
-            new_list.append()
-            x += 1
-        else:
-            pass
-    print('new_list')
+ def unique_list(lst):
+    return list(set(lst))
+unique_list ([1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 5])
 
-unique_List([1,1,1,1,2,2,3,3,3,3,4,5])
+
+def unique_list(lst):
+    seen_numbers=[]
+    for nb in lst:
+        if nb not in seen_numbers:
+            seen_numbers.append(nb)
+    return seen_numbers
+
+unique_list ([1,1,1,1,2,2,3,3,3,3,4,5])
+
