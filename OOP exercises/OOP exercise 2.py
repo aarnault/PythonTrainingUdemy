0f035e4911c4eb2ca74dc658da13a0fefd5b6604
audrey.arnault <audrey-arnault@web-address.fr>
2020-06-22 17:13:25 +0100

    end

--- OOP exercises/OOP exercise 2.py
+++ OOP exercises/OOP exercise 2.py
@@ -1,15 +1,15 @@
 class Cylinder:
-    pi = 3.14
 
     def __init__(self, height=1, radius=1):
         self.height = height
         self.radius = radius
 
     def volume(self):
-        return ((4/3)*volume.pi*(radius)**3)
+        return self.height * 3.14 * (self.radius) ** 2
 
     def surface_area(self):
-        return (surface.pi*(radius)**2)
+        top = 3.14 * (self.radius) ** 2
+        return (2 * top) + (2 * 3.14 * self.radius * self.height)
 
     c = Cylinder(2, 3)
 
