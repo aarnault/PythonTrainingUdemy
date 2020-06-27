585353fdaf2531821ee11fc7d067227cb2dc0df5
audrey.arnault <audrey-arnault@web-address.fr>
2020-06-27 12:38:06 +0100

    end

--- Error and exception/Exercise 3.py
+++ Error and exception/Exercise 3.py
@@ -1,9 +1,12 @@
-hile True:
-    try:
-        entry = int(input("Please provide an number:"))
-    except:
-        print("An error occured! Please provide a number to proceed:")
-    else:
-        entry = entry **2
-        print(f"Thank you, your number squarred is {entry}")
-        break
+def ask():
+    while True:
+        try:
+            entry = int(input("Please provide an number:"))
+        except:
+            print("An error occured! Please provide a number to proceed:")
+        else:
+            entry = entry **2
+            print(f"Thank you, your number squarred is {entry}")
+            break
+
+    ask()
