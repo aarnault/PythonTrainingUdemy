c5d1937fcb6b4c5e31e4f8c097382c1b64d94761
Audrey Arnault <audrey-arnault@web-address.fr>
2020-07-09 11:13:55 +0100

    end

--- Web scrapping/Grabbing info.py
+++ Web scrapping/Grabbing info.py
@@ -5,4 +5,33 @@ result = requests.get("https://example.com/")
 
 soup = bs4.BeautifulSoup(result.text, "lxml")
 
-soup.select("title")
+soup.select("title")[0].getText()
+site_paragraphs = soup.select("p")
+site_paragraphs[0].getText()
+
+res = requests.get('https://en.wikipedia.org/wiki/Grace_Hopper')
+soup = bs4.BeautifulSoup(res.text, "lxml")
+
+first_item = soup.select('.toctext')[0]
+first_time.text
+
+for item in soup.select('.tocext'):
+    print(item.text)
+
+
+res = requests.get ('https://en.wikipedia.org/wiki/Deep_Blue_%28chess_computer%29')
+soup = bs4.BeautifulSoup(res.text, "lxml")
+
+soup.select('.thumbimage')
+
+computer = soup.select('.thumbimage') [0]
+computer['src']
+<img src='//upload.wikimedia.org/wikipedia/commons/thumb/b/be/Deep_Blue.jpg/220px-Deep_Blue.jpg'>
+
+image_img = requests.get('//upload.wikimedia.org/wikipedia/commons/thumb/b/be/Deep_Blue.jpg/220px-Deep_Blue.jpg')
+
+f = open('my_computer_image.jpg', 'wb')
+f.write(image_link.content)
+
+f.close()
+
