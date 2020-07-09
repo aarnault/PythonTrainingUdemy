724dc2c2a565c38f6cb0802ba51bdf1318fcfeaa
Audrey Arnault <audrey-arnault@web-address.fr>
2020-07-09 13:59:52 +0100

    end works in Pycharm

--- Working with images/Image exercise.py
+++ Working with images/Image exercise.py
@@ -1,9 +1,9 @@
 from PIL import Image
 
-mask = Image.open('mask.png')
+mask = Image.open(r'mask.png')
 #mask.size # 505, 251
 
-words =Image.open('word_matrix.png')
+words =Image.open(r'word_matrix.png')
 #words.size # 1015, 559
 
 new_mask = mask.resize((1015, 559))
@@ -11,4 +11,4 @@ new_mask = mask.resize((1015, 559))
 new_mask.putalpha(200)
 words.paste(im=new_mask, box =(0,0), mask =new_mask)
 
-words
+words.show()
