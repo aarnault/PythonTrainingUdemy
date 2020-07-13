0c50b0727d5042ee65008ab20390d5eb290acc18
Audrey Arnault <audrey-arnault@web-address.fr>
2020-07-13 10:50:37 +0100

    Draft1

--- Working with csv/Course.py
+++ Working with csv/Course.py
@@ -1,3 +1,5 @@
+#CSV (Common S V)
+
 import csv
 
 #open the file
@@ -46,3 +48,49 @@ f=open('to_save_file.csv', mode='a', newline='')
 csv_writer = csv.writer(f)
 
 csv_writer.writerow(['1', '2', '3'])
+
+f.close()
+
+#PDF(Portable Document Format) needs to be readable by PyPDF2
+
+import PyPDF2
+
+f= open('working_Business_Proposal.pdf', 'rb')
+pdf_reader = PyPDF2.PdfFileReader(f)
+pdf_reader.numPages
+
+page_one=pdf_reader.getPage(0)
+page_one_text =page_one.extractText()
+page_one_text
+
+f.close()
+
+f=open('Working_Business_Proposal.pdf', 'rb')
+pdf_reader = PyPDF2.PdfFileReader(f)
+
+first_page = pdf_reader.getPage(0)
+
+pdf_writer = PyPDF2.PdfFileWriter()
+
+pdf_writer.addPage(first_page)
+
+#open a new file
+
+pdf_output = open('Some_BrandNew_Doc.pdf', 'wb')
+pdf_writer.write(pdf_output)
+
+f.close()
+
+pdf_output.close()
+
+#grab all the text from a pdf
+
+f=open('Working_Business_Proposal.pdf', 'rb')
+
+pdf_text = []
+pdf_reader = PyPDF2.PdfFileReader(f)
+
+for num in range(pdf_reader.numPages):
+    page =pdf_reader.getPage(num)
+    pdf_text.append(page.extractText())
+    print(pdf_text[1])
